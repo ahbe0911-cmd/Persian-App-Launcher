d3cc839476c45ddc89cd047b42ee4fd91c35f37c
ahbe0911-cmd <ahbe0911@gmail.com>
2026-09-10 23:00:14 +0330

    build: bump launcher to version 22

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.ahbe.mylauncher"
         minSdk = 26
         targetSdk = 35
-        versionCode = 21
-        versionName = "0.21.0"
+        versionCode = 22
+        versionName = "0.22.0"
     }
 
     buildFeatures { compose = true }
